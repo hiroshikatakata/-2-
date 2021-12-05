033d8ca4e4cb3d304d5477a28920f4250cddeea9
hiroshikatakata <93134765+hiroshikatakata@users.noreply.github.com>
2021-12-05 16:06:08 +0900

    演算子が連続した場合と0が先頭もしくは演算子の後に入力された場合の上書き処理追加

--- main2.js
+++ main2.js
@@ -10,6 +10,20 @@
         } else if (btn.value == "÷") {
           btn.value = "/";
         } 
+      let a=document.dentaku.display.value.slice( -1 )
+      //演算子が連続した場合
+      if(btn.value == "*" || btn.value == "/" || btn.value == "+" || btn.value == "-"){
+        if(a == "*" || a == "/" || a == "+" || a == "-"){
+          document.dentaku.display.value=document.dentaku.display.value.slice(0,-1)
+        }
+      }
+      let b=document.dentaku.display.value.slice(0,1)
+      let c=document.dentaku.display.value.slice(-2)
+      
+      //0が最初に入力、もしくは演算子の後に入力されていた場合
+      if( b == "0" || c == "*0" || c == "/0" || c == "+0" || c == "-0"){
+        document.dentaku.display.value=document.dentaku.display.value.slice(0,-1)
+      } 
         document.dentaku.display.value += btn.value;
         document.dentaku.multi_btn.value = "×";
         document.dentaku.div_btn.value = "÷";
